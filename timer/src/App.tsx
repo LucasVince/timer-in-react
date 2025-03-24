f4bed38122115baa4093f25c397d4d421ca28e44
Lucas Vince <lucashenriquevince1714@gmail.com>
2025-03-24 09:08:49 -0300

    feat: finished the get value from the inputs

--- timer/src/App.tsx
+++ timer/src/App.tsx
@@ -1,13 +1,21 @@
 import InputTime from "./components/InputTime";
 
 const App = () => {
+  const onMinChange = (value:number) => {
+    console.log(value)
+  }
+  
+  const onSecChange = (value:number) => {
+    console.log(value)
+  }
+
   return (
     <div className="bg-slate-600 w-screen h-screen flex items-center justify-center">
       <div className="bg-slate-200 p-5 rounded-md flex flex-col gap-5">
         <h1 className="text-4xl text-slate-700 font-bold text-center">Timer</h1>
         <div className="flex gap-5">
-          <InputTime placeholder="min"/>
-          <InputTime placeholder="sec"/>
+          <InputTime placeholder="min" timestamp="min" onMinChange={onMinChange}/>
+          <InputTime placeholder="sec" timestamp="sec" onSecChange={onSecChange}/>
         </div>
         <button className="bg-slate-500 text-slate-200 p-1 rounded-md duration-75 hover:bg-slate-800">
           start
--- timer/src/components/InputTime.tsx
+++ timer/src/components/InputTime.tsx
@@ -1,13 +1,36 @@
-import {FunctionComponent} from "react"
+import {FunctionComponent, useState} from "react"
 
 interface InterfaceInputTime {
     placeholder: string;
+    timestamp: 'min' | 'sec';
+    onMinChange?: (value: number) => void;
+    onSecChange?: (value: number) => void;
 }
 
-const InputTime:FunctionComponent<InterfaceInputTime> = ({placeholder}) => {
+const InputTime:FunctionComponent<InterfaceInputTime> = ({placeholder, timestamp, onMinChange, onSecChange}) => {
+    const [min, setMin] = useState<number>(0);
+    const [sec, setSec] = useState<number>(0);
+
     return(
-        <input max={60} min={0} placeholder={placeholder} type="number"
-        className="p-1 rounded-md outline-none text-center"
+        <input max={60} min={0} placeholder={placeholder} data-timestamp={timestamp} type="number"
+            className="p-1 rounded-md outline-none text-center"
+            onChange={(event) => {
+                if (timestamp == 'min') {
+                    setMin(Number(event.target.value));
+                    if (onMinChange) {
+                        onMinChange(min);
+                    }
+                }
+                if (timestamp == 'sec') {
+                    setSec(Number(event.target.value));
+                    if (onSecChange) {
+                        onSecChange(sec);
+                    }
+                }
+            }}
+            onKeyDown={(event) => {
+                event.preventDefault();
+            }}
         />
     );
 }
