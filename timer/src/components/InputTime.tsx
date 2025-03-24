adf3ce8505952d209faf89b092d45c85c94fe76b
Lucas Vince <lucashenriquevince1714@gmail.com>
2025-03-24 07:45:08 -0300

    chore: seted up the clned repository

--- timer/src/components/InputTime.tsx
+++ timer/src/components/InputTime.tsx
@@ -7,7 +7,7 @@ interface InterfaceInputTime {
 const InputTime:FunctionComponent<InterfaceInputTime> = ({placeholder}) => {
     return(
         <input max={60} min={0} placeholder={placeholder} type="number"
-        className="p-1 rounded-md outline-none"
+        className="p-1 rounded-md outline-none text-center"
         />
     );
 }
